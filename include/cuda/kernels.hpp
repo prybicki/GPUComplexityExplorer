830f1ebea8cda273288c9be6a8853a60d4ec9e5f
Piotr Rybicki <piotr.rybicki@robotec.ai>
2021-11-20 20:53:11 +0100

    Add a few basic kernels, move kernels.hpp to cuda/

---
+++ include/cuda/kernels.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cuda_runtime.h>
+#include <Vector.hpp>
+#include <Matrix.hpp>
+
+using count_t = int;
+
+__global__ void kHelloWorld(int value);
+__global__ void kPos2DToTransform3x3(count_t count, const Vec2f* position, const float* radius, Mat3x3f* outTransform);
+__global__ void kApplyVelocity(count_t count, float dt, const Vec2f* vel, Vec2f* pos);
--- include/kernels.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <cuda_runtime.h>
-
-__global__ void kHelloWorld(int value);
