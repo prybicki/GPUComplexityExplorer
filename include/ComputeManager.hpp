b4546deb73bfa5b1d54d3ef4cfa2b3f85a31be3d
Piotr Rybicki <piotr.rybicki@robotec.ai>
2021-11-20 20:51:45 +0100

    Add ComputeManager stub + internal/macros.hpp

---
+++ include/ComputeManager.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+struct ComputeManager
+{
+	static ComputeManager& instance()
+	{
+		static ComputeManager instance;
+		return instance;
+	}
+
+	template<typename Kernel, typename... Args>
+	void runSync1D(int totalThreadCount, int blockThreadCount, Kernel&& kernel, Args... args) {
+		void* kernelPtr = reinterpret_cast<void*>(&kernel);
+		std::vector<void*> argsVec = intoPointers(args...);
+		dim3 gridDim = {1U + totalThreadCount / blockThreadCount};
+		dim3 blockDim = {static_cast<unsigned>(blockThreadCount), 1, 1};
+
+		CHECK_CUDA(cudaLaunchKernel(kernelPtr, gridDim, blockDim, argsVec.data(), 0, 0));
+		CHECK_CUDA(cudaStreamSynchronize(nullptr));
+	}
+
+private:
+	ComputeManager() {}
+
+	template<typename T, typename... Ts>
+	std::vector<void*> intoPointers(T& arg, Ts&... args)
+	{
+		if constexpr (sizeof...(args) == 0) {
+			return {&arg};
+		}
+		else {
+			std::vector<void*> head = {&arg};
+			std::vector<void*> tail = intoPointers(args...);
+			head.insert(head.end(), tail.begin(), tail.end());
+			return head;
+		}
+	}
+};
+
+ComputeManager& cm = ComputeManager::instance();
--- include/internal/macros.hpp
+++ include/internal/macros.hpp
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <vector>
-
 #define CHECK_CUDA(call)                                                                            \
 do                                                                                                  \
 {                                                                                                   \
@@ -13,28 +11,13 @@ do                                                                              
 }                                                                                                   \
 while(false)
 
-
-template<typename T, typename... Ts>
-std::vector<void*> intoPointers(T& arg, Ts&... args)
-{
-	if constexpr (sizeof...(args) == 0) {
-		return {&arg};
-	}
-	else {
-		std::vector<void*> head = {&arg};
-		std::vector<void*> tail = intoPointers(args...);
-		head.insert(head.end(), tail.begin(), tail.end());
-		return head;
-	}
-}
-
-template<typename Kernel, typename... Args>
-void runSync1D(int totalThreadCount, int blockThreadCount, Kernel&& kernel, Args... args) {
-	void* kernelPtr = reinterpret_cast<void*>(&kernel);
-	std::vector<void*> argsVec = intoPointers(args...);
-	dim3 gridDim = {1U + totalThreadCount / blockThreadCount};
-	dim3 blockDim = {static_cast<unsigned>(blockThreadCount), 1, 1};
-
-	CHECK_CUDA(cudaLaunchKernel(kernelPtr, gridDim, blockDim, argsVec.data(), 0, 0));
-	CHECK_CUDA(cudaStreamSynchronize(nullptr));
-}
+#define CHECK_CUDA_NO_THROW(call)                                                           \
+do                                                                                          \
+{                                                                                           \
+	cudaError_t status = call;                                                              \
+	if (status != cudaSuccess) {                                                            \
+		fmt::print(stderr, "cuda error: {} (code={})", cudaGetErrorString(status), status); \
+		std::exit(EXIT_FAILURE);                                                            \
+	}                                                                                       \
+}                                                                                           \
+while(false)
