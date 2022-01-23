88929325eb6d7979061f4381dce69f517fed7ad2
Piotr Rybicki <piotr.rybicki@robotec.ai>
2022-01-23 16:00:50 +0100

    Add ImPlot to build

--- cmake/ConfigureMagnum.cmake
+++ cmake/ConfigureMagnum.cmake
@@ -18,11 +18,20 @@ set(WITH_STBIMAGEIMPORTER ON CACHE BOOL "" FORCE)
 set(BUILD_PLUGINS_STATIC ON CACHE BOOL "" FORCE)
 add_subdirectory(external/magnum-plugins EXCLUDE_FROM_ALL)
 
-# Integration
+# Integration (ImGui)
 set(WITH_IMGUI ON CACHE BOOL "" FORCE)
 set(IMGUI_DIR "${CMAKE_SOURCE_DIR}/external/imgui")
 set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${PROJECT_SOURCE_DIR}/external/magnum-integration/modules")
 add_subdirectory(external/magnum-integration EXCLUDE_FROM_ALL)
+
+# ImPlot
+#include_directories(external/implot)
+add_library(ImPlot
+	external/implot/implot.cpp
+	external/implot/implot_items.cpp)
+target_include_directories(ImPlot INTERFACE external/implot)
+target_include_directories(ImPlot PRIVATE external/imgui)
+
 
 # Remove unwanted hierarchy in the build directory introduced by Magnum
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
